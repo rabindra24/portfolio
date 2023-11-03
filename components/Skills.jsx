4d17787fc24fc24e5525355048309206eae157eb
rabin2403 <rabindra2402@gmail.com>
2023-11-03 23:35:41 +0530

    new

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,7 +8,7 @@ const getData = async () => {
   const client = createClient({
     projectId: "f3is13ck",
     dataset: "production",
-    apiVersion: '2021-03-25',
+    apiVersion: '2021-10-21',
     useCdn: true,
   });
 
